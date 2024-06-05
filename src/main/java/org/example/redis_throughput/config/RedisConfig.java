fad56d89ba5fe7e35fdf0d592e17c4d25580842c
Vladislav <v.r.ryazantsev@gmail.com>
2024-06-05 12:06:47 +0300

    apply jedis connection pool

--- src/main/java/org/example/redis_throughput/config/RedisConfig.java
+++ src/main/java/org/example/redis_throughput/config/RedisConfig.java
@@ -1,14 +1,26 @@
 package org.example.redis_throughput.config;
 
 import org.example.redis_throughput.dto.PayloadDto;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    RedisConnectionFactory jedisConnectionFactory(RedisProperties properties) {
+        var config = new RedisStandaloneConfiguration();
+        config.setPassword(properties.getPassword());
+        config.setHostName(properties.getHost());
+        config.setPort(properties.getPort());
+        return new JedisConnectionFactory(config);
+    }
 
     @Bean("template")
     RedisTemplate<String, PayloadDto> redisTemplate(RedisConnectionFactory connectionFactory, RedisPayloadValueSerializer serializer) {
